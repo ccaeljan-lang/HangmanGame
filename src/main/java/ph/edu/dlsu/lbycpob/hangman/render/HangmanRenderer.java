a26728e6c7b294f15eab8821459bbde6046cc8c3
caeljandcristobal <ccaeljan@gmail.com>
2026-07-08 19:55:27 +0800

    Renamed parameter in render abstract method.

--- src/main/java/ph/edu/dlsu/lbycpob/hangman/render/HangmanRenderer.java
+++ src/main/java/ph/edu/dlsu/lbycpob/hangman/render/HangmanRenderer.java
@@ -16,5 +16,5 @@ public interface HangmanRenderer {
      */
     // [UNDERSTAND] Displays the current structure of the hangman based on the current guess.
     // Reading a text file can throw an IOException, so the interface must declare it
-    void render(int guessesRemaining) throws IOException;
+    void render(int guessesCount) throws IOException;
 }
